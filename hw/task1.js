3a25b07730a026f4135e67041479be87df140e8a
Maksim Alipau <maxanotest@gmail.com>
2025-04-09 20:11:07 +0300

    feat: add a task 1 part 1

---
+++ hw/task1.js
@@ -0,0 +1,14 @@
+// Изучение JSON
+// Создайте простой JSON-объект, представляющий информацию о пользователе (имя, возраст, адрес и др.).
+
+const user = {
+    name: 'Maksim',
+    age: 36,
+    address: {
+        city: 'Minks',
+        street: 'Main Street',
+        number: 123
+    }
+}
+
+console.log(user);
